ae0e7e238ed99005e2ae869a22756c308aa45cb4
Ivan Dardi <ivy.dardi@gmail.com>
2017-09-03 21:23:20 -0300

    Add command line interface for compiling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 extern crate compiler;
 
-fn main() {}
+use compiler::{compile, get_code};
+
+use std::env;
+
+fn main() {
+    let filename = env::args().nth(1).expect("Expected file to compile");
+
+    println!("Compiling {:?}", filename);
+
+    let code = get_code(&filename).expect("Failed to get code");
+    let results = compile(&code);
+
+    if let Err(results) = results {
+        panic!("Error: {}", results);
+    }
+}
